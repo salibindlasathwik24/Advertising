8dd59be25e74ea7b0bfa4e5f8c32a65c21ace4b5
edwina sweeney <e-sweeney@users.noreply.github.com>
2025-12-16 11:30:21 +0000

    Implement training and saving of Linear Regression model

---
+++ train.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+
+# Load dataset (Example: Insurance Charges Dataset. Remove first column index)
+df = pd.read_csv("advertising.csv")
+
+
+# Features (X) and Target (y)
+X = df[["TV", "Radio",  "Newspaper"]]
+y = df["Sales"]
+
+# Split data into training and testing sets (80% train, 20% test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train Linear Regression Model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Save model to a .pkl file
+with open("model.pkl", "wb") as file:
+    pickle.dump(model, file)
+
+print("Model trained and saved as model.pkl!")
